d285ac48ea31545f5eb403bc7d073f491da7e1af
Malesela Sithole <malesela.ms@gmail.com>
2025-10-27 00:50:13 +0200

    setup commit

---
+++ src/main/java/com/red_badger/Martian/Robots/MartianRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.red_badger.Martian.Robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartianRobotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartianRobotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/red_badger/Martian/Robots/MartianRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.red_badger.Martian.Robots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartianRobotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
